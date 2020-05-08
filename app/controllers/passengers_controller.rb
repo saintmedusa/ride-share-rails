5b1f19eafcee5678e407c86e70cd5db82d83831a
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-07 20:16:44 -0700

    Added passenger details link and show view (passenger details page)

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,13 @@ class PassengersController < ActionController::Base
   def index
     @passengers = Passenger.all
   end
+
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+
+    if @passenger.nil?
+      redirect_to index_path
+    end  
+  end
 end
