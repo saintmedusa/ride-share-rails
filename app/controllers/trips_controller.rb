38e59852f9c49191a2946c3e03341bf336f052de
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-08 17:23:35 -0700

    Added new trip link on passenger page (to placeholder)

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,4 +11,66 @@ class TripsController < ActionController::Base
       redirect_to index_path
     end  
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+      return
+    elsif @trip.update(trip_params)
+      redirect_to trip_path 
+      return
+    else 
+      render :edit
+      return
+    end
+  end
+  
+  def edit
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+
+    if @trip.nil?
+      head :not_found
+      return
+    end  
+  end
+
+  #FIX
+  def destroy
+    trip_id = params[:id]
+    @trip = Trip.find_by(id: trip_id)
+
+    if @trip.nil?
+      head :not_found
+      return
+    elsif 
+      @trip.destroy
+      redirect_to trip_path 
+      return
+    else 
+      render @trip
+      return
+    end
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+
+    if @trip.save
+      redirect_to @trip
+    else
+      render :new, :bad_request
+    end
+  end
+
+  private 
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost)
+  end
 end
