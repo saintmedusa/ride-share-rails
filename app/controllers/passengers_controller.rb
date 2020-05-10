c454ac27634c1f985134774b928bca08231bf1d6
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-10 10:33:15 -0700

    Updated passenger show and delete functions -- added delete confirmation step and fixed faulty delete function

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -8,7 +8,8 @@ class PassengersController < ApplicationController
     @passenger = Passenger.find_by(id: passenger_id)
 
     if @passenger.nil?
-      redirect_to index_path
+      head :not_found
+      return
     end  
   end
 
@@ -42,29 +43,13 @@ class PassengersController < ApplicationController
       head :not_found
       return
     elsif @passenger.update(passenger_params)
-      redirect_to passenger_path 
+      redirect_to @passenger 
       return
     else 
       render :edit, :bad_request
       return
     end
   end
-
-  #FIX
-  # def destroy
-  #   @passenger = Passenger.find_by(id: params[:id])
-  #   if @passenger.nil?
-  #     head :not_found
-  #     return 
-  #   end
-
-  #   @passenger.destroy
-    
-  #   redirect_to passengers_path
-    
-  #   return 
-  # end
-
 
   def destroy
     passenger = Passenger.find_by(id: params[:id])
